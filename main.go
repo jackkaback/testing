57e2da07d82d14b9d34b02e35ba8ea7988588699
jack <jackkaback@gmail.com>
2017-12-09 13:23:33 -0500

    testing vars

--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 import "fmt"
 
 func main() {
+
+	// implicit setting a var
 	temp := 20
 
+	// semi-implicit setting a var
+	var t = 0
+
+
+	// explicit setting a var
+	var b int = 30
+
+	fmt.Println(b)
 	fmt.Println("hello world")
 	fmt.Print(temp)
-	for j := 0; j <= 9; j++ {
-		fmt.Println(j)
+	for t = 0; t <= 9; t++ {
+		fmt.Println(t)
 	}
 }
